foo86/dcadec

commit 84b77a1bbb9a7eed6354dd5aeb618708f2318673

Add frame syncing and bitstream conversion APIs.
Also add support for reading 14-bit DTS streams packed into 16-bit words.
Fixes #23.

libdcadec/dca_stream.c
@@ -18,6 +18,7 @@
 
 #include "common.h"
 #include "bitstream.h"
+#include "dca_frame.h"
 #include "dca_stream.h"
 
 #ifdef _WIN32
@@ -28,12 +29,18 @@
 #endif
 
 #define BUFFER_ALIGN    4096
-#define HEADER_SIZE     16
-#define SYNC_SIZE       4
 
 #define AUPR_HDR    UINT64_C(0x415550522D484452)
 #define DTSHDHDR    UINT64_C(0x4454534844484452)
 #define STRMDATA    UINT64_C(0x5354524D44415441)
+
+#if (defined _WIN32)
+#define DCA_FGETC   _fgetc_nolock
+#elif (defined _BSD_SOURCE)
+#define DCA_FGETC   fgetc_unlocked
+#else
+#define DCA_FGETC   fgetc
+#endif
 
 struct dcadec_stream {
     FILE    *fp;
@@ -195,6 +202,7 @@ DCADEC_API void dcadec_stream_close(struct dcadec_stream *stream)
 
 static uint8_t *prepare_packet_buffer(struct dcadec_stream *stream, size_t size)
 {
+    size = DCA_ALIGN(size, DCADEC_FRAME_BUFFER_ALIGN);
     size += stream->packet_size + DCADEC_BUFFER_PADDING;
     size = DCA_ALIGN(size, BUFFER_ALIGN);
 
@@ -207,24 +215,12 @@ static uint8_t *prepare_packet_buffer(struct dcadec_stream *stream, size_t size)
     return NULL;
 }
 
-static void swap16(uint32_t *data, size_t size)
-{
-    while (size--) {
-        uint32_t v = *data;
-        *data++ = ((v & 0x00ff00ff) << 8) | ((v & 0xff00ff00) >> 8);
-    }
-}
-
-#if (defined _WIN32)
-#define DCA_FGETC   _fgetc_nolock
-#elif (defined _BSD_SOURCE)
-#define DCA_FGETC   fgetc_unlocked
-#else
-#define DCA_FGETC   fgetc
-#endif
-
 static int read_frame(struct dcadec_stream *stream, uint32_t *sync_p)
 {
+    uint8_t header[DCADEC_FRAME_HEADER_SIZE], *buf;
+    size_t frame_size;
+    int ret;
+
     // Stop at position indicated by STRMDATA if known
     if (stream->stream_end > 0 && ftello(stream->fp) >= stream->stream_end)
         return 0;
@@ -235,7 +231,9 @@ static int read_frame(struct dcadec_stream *stream, uint32_t *sync_p)
     while (sync != SYNC_WORD_CORE
         && sync != SYNC_WORD_EXSS
         && sync != SYNC_WORD_CORE_LE
-        && sync != SYNC_WORD_EXSS_LE) {
+        && sync != SYNC_WORD_EXSS_LE
+        && sync != SYNC_WORD_CORE_LE14
+        && sync != SYNC_WORD_CORE_BE14) {
         int c = DCA_FGETC(stream->fp);
         if (c == EOF)
             return 0;
@@ -252,81 +250,37 @@ static int read_frame(struct dcadec_stream *stream, uint32_t *sync_p)
     // Clear backed up sync word
     stream->backup_sync = 0;
 
-    // Reallocate packet buffer
-    uint8_t *buf;
-    if (!(buf = prepare_packet_buffer(stream, HEADER_SIZE)))
-        return -DCADEC_ENOMEM;
+    // Restore sync word
+    header[0] = (sync >> 24) & 0xff;
+    header[1] = (sync >> 16) & 0xff;
+    header[2] = (sync >>  8) & 0xff;
+    header[3] = (sync >>  0) & 0xff;
 
     // Read the frame header
-    if (fread(buf + SYNC_SIZE, HEADER_SIZE - SYNC_SIZE, 1, stream->fp) != 1)
+    if (fread(header + 4, sizeof(header) - 4, 1, stream->fp) != 1)
         return 0;
 
-    bool swap = false;
-    switch (sync) {
-    case SYNC_WORD_CORE_LE:
-        sync = SYNC_WORD_CORE;
-        swap = true;
-        break;
-    case SYNC_WORD_EXSS_LE:
-        sync = SYNC_WORD_EXSS;
-        swap = true;
-        break;
-    }
-
-    if (swap)
-        swap16((uint32_t *)buf, HEADER_SIZE / 4);
-
-    struct bitstream bits;
-
-    bits_init(&bits, buf + SYNC_SIZE, HEADER_SIZE - SYNC_SIZE);
-
-    size_t frame_size;
-
-    if (sync == SYNC_WORD_CORE) {
-        bool normal_frame = bits_get1(&bits);
-        int deficit_samples = bits_get(&bits, 5) + 1;
-        if (normal_frame && deficit_samples != 32)
-            return -DCADEC_ENOSYNC;
-        bits_skip1(&bits);
-        int npcmblocks = bits_get(&bits, 7) + 1;
-        if (npcmblocks < 6)
-            return -DCADEC_ENOSYNC;
-        frame_size = bits_get(&bits, 14) + 1;
-        if (frame_size < 96)
-            return -DCADEC_ENOSYNC;
-    } else {
-        bits_skip(&bits, 10);
-        bool wide_hdr = bits_get1(&bits);
-        bits_skip(&bits, 8 + 4 * wide_hdr);
-        frame_size = bits_get(&bits, 16 + 4 * wide_hdr) + 1;
-        if (frame_size < HEADER_SIZE)
-             return -DCADEC_ENOSYNC;
-    }
-
-    // Align frame size to 4-byte boundary
-    size_t aligned_size = DCA_ALIGN(frame_size, 4);
+    // Parse and validate the frame header
+    if ((ret = dcadec_frame_parse_header(header, &frame_size)) < 0)
+        return ret;
 
     // Reallocate packet buffer
-    if (!(buf = prepare_packet_buffer(stream, aligned_size)))
+    if (!(buf = prepare_packet_buffer(stream, frame_size)))
         return -DCADEC_ENOMEM;
 
+    // Restore frame header
+    memcpy(buf, header, sizeof(header));
+
     // Read the rest of the frame
-    if (fread(buf + HEADER_SIZE, frame_size - HEADER_SIZE, 1, stream->fp) != 1)
+    if (fread(buf + sizeof(header), frame_size - sizeof(header), 1, stream->fp) != 1)
         return 0;
 
-    if (swap)
-        swap16((uint32_t *)(buf + HEADER_SIZE), (aligned_size - HEADER_SIZE) / 4);
+    // Convert the frame in place
+    if ((ret = dcadec_frame_convert_bitstream(buf, &frame_size, buf, frame_size)) < 0)
+        return ret;
 
-    stream->packet_size += aligned_size;
-
-    // Shut up memcheck
-    memset(buf + frame_size, 0, aligned_size - frame_size + DCADEC_BUFFER_PADDING);
-
-    // Restore sync word
-    buf[0] = (sync >> 24) & 0xff;
-    buf[1] = (sync >> 16) & 0xff;
-    buf[2] = (sync >>  8) & 0xff;
-    buf[3] = (sync >>  0) & 0xff;
+    // Align frame size to 4-byte boundary
+    stream->packet_size += DCA_ALIGN(frame_size, 4);
 
     if (sync_p)
         *sync_p = sync;

Makefile
@@ -52,8 +52,10 @@ INC_LIB = \
 libdcadec/dca_context.h
 
 ifndef CONFIG_SMALL
+SRC_LIB += libdcadec/dca_frame.c
 SRC_LIB += libdcadec/dca_stream.c
 SRC_LIB += libdcadec/dca_waveout.c
+INC_LIB += libdcadec/dca_frame.h
 INC_LIB += libdcadec/dca_stream.h
 INC_LIB += libdcadec/dca_waveout.h
 endif

dcadec.c
@@ -219,7 +219,7 @@ int main(int argc, char **argv)
     }
 
     if (ret == 0) {
-        fprintf(stderr, "This doesn't look like a 16-bit DTS bit stream\n");
+        fprintf(stderr, "This doesn't look like a valid DTS bit stream\n");
         dcadec_stream_close(stream);
         return 1;
     }

libdcadec/common.h
@@ -195,6 +195,8 @@ enum Speaker {
 enum SyncWord {
     SYNC_WORD_CORE      = 0x7ffe8001,
     SYNC_WORD_CORE_LE   = 0xfe7f0180,
+    SYNC_WORD_CORE_LE14 = 0xff1f00e8,
+    SYNC_WORD_CORE_BE14 = 0x1fffe800,
     SYNC_WORD_REV1AUX   = 0x9a1105a0,
     SYNC_WORD_REV2AUX   = 0x7004c070,
     SYNC_WORD_XCH       = 0x5a5a5a5a,

libdcadec/dca_frame.c
@@ -1,0 +1,144 @@
+/*
+ * This file is part of libdcadec.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "common.h"
+#include "bitstream.h"
+#include "dca_frame.h"
+
+#define SRC_OP(E) \
+    uint16_t src_0 = DCA_16##E(_src[0]); \
+    uint16_t src_1 = DCA_16##E(_src[1]); \
+    uint16_t src_2 = DCA_16##E(_src[2]); \
+    uint16_t src_3 = DCA_16##E(_src[3]); \
+    uint16_t src_4 = DCA_16##E(_src[4]); \
+    uint16_t src_5 = DCA_16##E(_src[5]); \
+    uint16_t src_6 = DCA_16##E(_src[6]); \
+    uint16_t src_7 = DCA_16##E(_src[7]);
+
+#define DST_OP \
+    _dst[0] = DCA_16BE((src_0 <<  2) | ((src_1 & 0x3fff) >> 12)); \
+    _dst[1] = DCA_16BE((src_1 <<  4) | ((src_2 & 0x3fff) >> 10)); \
+    _dst[2] = DCA_16BE((src_2 <<  6) | ((src_3 & 0x3fff) >>  8)); \
+    _dst[3] = DCA_16BE((src_3 <<  8) | ((src_4 & 0x3fff) >>  6)); \
+    _dst[4] = DCA_16BE((src_4 << 10) | ((src_5 & 0x3fff) >>  4)); \
+    _dst[5] = DCA_16BE((src_5 << 12) | ((src_6 & 0x3fff) >>  2)); \
+    _dst[6] = DCA_16BE((src_6 << 14) | ((src_7 & 0x3fff) >>  0));
+
+DCADEC_API int dcadec_frame_convert_bitstream(uint8_t *dst, size_t *dst_size,
+                                              const uint8_t *src, size_t src_size)
+{
+    const uint16_t *_src = (const uint16_t *)src;
+    uint16_t *_dst = (uint16_t *)dst;
+    size_t count;
+
+    switch (DCA_MEM32NE(src)) {
+    case DCA_32BE(SYNC_WORD_CORE):
+    case DCA_32BE(SYNC_WORD_EXSS):
+        if (src != dst)
+            memcpy(dst, src, src_size);
+        *dst_size = src_size;
+        return DCADEC_BITSTREAM_BE16;
+
+    case DCA_32BE(SYNC_WORD_CORE_LE):
+    case DCA_32BE(SYNC_WORD_EXSS_LE):
+        count = (src_size + 1) / 2;
+        while (count--)
+            *_dst++ = dca_bswap16(*_src++);
+        *dst_size = src_size;
+        return DCADEC_BITSTREAM_LE16;
+
+    case DCA_32BE(SYNC_WORD_CORE_BE14):
+        count = (src_size + 15) / 16;
+        while (count--) {
+            SRC_OP(BE)
+            DST_OP
+            _src += 8;
+            _dst += 7;
+        }
+        *dst_size = src_size - src_size / 8;
+        return DCADEC_BITSTREAM_BE14;
+
+    case DCA_32BE(SYNC_WORD_CORE_LE14):
+        count = (src_size + 15) / 16;
+        while (count--) {
+            SRC_OP(LE)
+            DST_OP
+            _src += 8;
+            _dst += 7;
+        }
+        *dst_size = src_size - src_size / 8;
+        return DCADEC_BITSTREAM_LE14;
+
+    default:
+        return -DCADEC_ENOSYNC;
+    }
+}
+
+#undef SRC_OP
+#undef DST_OP
+
+DCADEC_API int dcadec_frame_parse_header(const uint8_t *data, size_t *size)
+{
+    struct bitstream bits;
+    uint8_t header[DCADEC_FRAME_HEADER_SIZE];
+    size_t header_size, frame_size;
+
+    int ret;
+    if ((ret = dcadec_frame_convert_bitstream(header, &header_size,
+                                              data, DCADEC_FRAME_HEADER_SIZE)) < 0)
+        return ret;
+
+    bits_init(&bits, header, header_size);
+
+    switch (bits_get(&bits, 32)) {
+    case SYNC_WORD_CORE: {
+        bool normal_frame = bits_get1(&bits);
+        int deficit_samples = bits_get(&bits, 5) + 1;
+        if (normal_frame && deficit_samples != 32)
+            return -DCADEC_ENOSYNC;
+        bits_skip1(&bits);
+        int npcmblocks = bits_get(&bits, 7) + 1;
+        if (npcmblocks < 6)
+            return -DCADEC_ENOSYNC;
+        frame_size = bits_get(&bits, 14) + 1;
+        if (frame_size < 96)
+            return -DCADEC_ENOSYNC;
+        if (ret & DCADEC_BITSTREAM_BE14)
+            *size = frame_size + frame_size / 7;
+        else
+            *size = frame_size;
+        return DCADEC_FRAME_TYPE_CORE;
+    }
+
+    case SYNC_WORD_EXSS: {
+        bits_skip(&bits, 10);
+        bool wide_hdr = bits_get1(&bits);
+        bits_skip(&bits, 8 + 4 * wide_hdr);
+        frame_size = bits_get(&bits, 16 + 4 * wide_hdr) + 1;
+        if (frame_size < DCADEC_FRAME_HEADER_SIZE)
+            return -DCADEC_ENOSYNC;
+        if (frame_size & 3)
+            return -DCADEC_ENOSYNC;
+        *size = frame_size;
+        return DCADEC_FRAME_TYPE_EXSS;
+    }
+
+    default:
+        return -DCADEC_ENOSYNC;
+    }
+}

libdcadec/dca_frame.h
@@ -1,0 +1,88 @@
+/*
+ * This file is part of libdcadec.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef DCA_FRAME_H
+#define DCA_FRAME_H
+
+#include "dca_context.h"
+
+/**
+ * Minimum size alignment, in bytes, of source and destination buffers that must
+ * be passed to dcadec_frame_convert_bitstream().
+ */
+#define DCADEC_FRAME_BUFFER_ALIGN   16
+
+/**
+ * Minimum size of data buffer, in bytes, that must be passed to
+ * dcadec_frame_parse_header().
+ */
+#define DCADEC_FRAME_HEADER_SIZE    16
+
+/**@{*/
+#define DCADEC_BITSTREAM_BE16   0
+#define DCADEC_BITSTREAM_LE16   1
+#define DCADEC_BITSTREAM_BE14   2
+#define DCADEC_BITSTREAM_LE14   3
+/**@}*/
+
+/**@{*/
+#define DCADEC_FRAME_TYPE_CORE  0   /**< Backward compatible DTS core */
+#define DCADEC_FRAME_TYPE_EXSS  1   /**< Extension sub-stream (EXSS) */
+/**@}*/
+
+/**
+ * Convert the raw input frame into native 16-bit big-endian format understood
+ * by dcadec_context_parse(). Can operate in-place when destination buffer
+ * is the same as source buffer.
+ *
+ * @param dst       Pointer to destination buffer. Destination buffer size
+ *                  must be no less than source buffer size, including
+ *                  alignment.
+ *
+ * @param dst_size  Filled with resulting frame size after conversion, in bytes.
+ *
+ * @param src       Pointer to source buffer that must start with a valid sync
+ *                  word. Source buffer size must be no less than src_size bytes
+ *                  plus alignment to DCADEC_FRAME_BUFFER_ALIGN boundary.
+ *
+ * @param src_size  Size of raw frame data prior to conversion, in bytes. Size
+ *                  should not include aligment.
+ *
+ * @return          Detected bitstream format on success, negative error code
+ *                  on failure.
+ */
+DCADEC_API int dcadec_frame_convert_bitstream(uint8_t *dst, size_t *dst_size,
+                                              const uint8_t *src, size_t src_size);
+
+/**
+ * Check that the passed data buffer starts with a valid sync word and satisfies
+ * the basic requirements to be a valid DTS core or EXSS frame header.
+ *
+ * @param data      Pointer to data buffer that possibly contains the frame
+ *                  header. The first DCADEC_FRAME_HEADER_SIZE bytes are read
+ *                  from the buffer.
+ *
+ * @param size      Filled with raw frame size, in bytes. Frame size includes
+ *                  sync word and header bytes.
+ *
+ * @return          Detected frame type on success, negative error code
+ *                  on failure.
+ */
+DCADEC_API int dcadec_frame_parse_header(const uint8_t *data, size_t *size);
+
+#endif

commit b86520e508ab6487b4f10f4009c60fa671cbe8b9

Fix parsing little-endian core plus EXSS streams.
Although such kind of streams are only theoretical possibility, restore
parsing of them broken since 84b77a1. Don't bother doing the same for
14-bit streams.

libdcadec/dca_stream.c
@@ -309,7 +309,7 @@ DCADEC_API int dcadec_stream_read(struct dcadec_stream *stream, uint8_t **data, 
 
     // Check for EXSS that may follow core frame and try to concatenate both
     // frames into single packet
-    if (sync == SYNC_WORD_CORE) {
+    if (sync == SYNC_WORD_CORE || sync == SYNC_WORD_CORE_LE) {
         ret = read_frame(stream, NULL);
         if (ret < 0 && ret != -DCADEC_ENOSYNC)
             return ret;